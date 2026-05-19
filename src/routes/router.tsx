7e9e2104e613d28d837b5b7d156717bfa1c2262c
Jayvee <jayvee.github0703@gmail.com>
2026-05-19 12:54:30 +0800

    Client Layout

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,9 +1,35 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter, Navigate } from "react-router-dom";
 import LoginPage from "@/pages/login";
+import ClientLayout from "@/pages/client/layouts/ClientLayout";
+import Dashboard from "@/pages/client/dasboard/Dashboard";
+import Project from "@/pages/client/project/Project";
+import Ticket from "@/pages/client/ticket/Ticket";
 
 export const Routes = createBrowserRouter([
     {
         path:"/",
         element:<LoginPage/>
+    },
+    {
+        path:"/client",
+        element:<ClientLayout/>,
+        children:[
+            {
+                index:true,
+                element:<Navigate to="/client/dashboard" replace/>
+            },
+            {
+                path:"dashboard",
+                element:<Dashboard/>
+            },
+            {
+                path:"project",
+                element:<Project/>
+            },
+            {
+                path:"ticket",
+                element:<Ticket/>
+            },
+        ]
     }
 ])
